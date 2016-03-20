0becb239eeafae19b2838e413911fe80ad2518ea
Sarah Bien <sarahbien@Sarahs-Air.wv.cc.cmu.edu>
2016-03-20 10:37:20 -0400

    testing changing models.py on master branch

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+# Create your models here
 
 class User(models.Model):
     name = models.CharField(max_length=50, default="")
