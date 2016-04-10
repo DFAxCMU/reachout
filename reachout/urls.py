3dbf8184ea85f9a863bcb30e6d420788ec9583c3
ljboard <ljboard@gmail.com>
2016-04-10 09:48:56 -0400

    search page

--- reachout/urls.py
+++ reachout/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^profiles/', include('profiles.urls')),
+    url(r'^search/', include('search.urls')),
     url(r'^admin/', admin.site.urls),
 ]
