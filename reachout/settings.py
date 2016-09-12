d40bdd8ca09f0090b9a427fc4efef5c5e871da9d
Sarah Bien <sarahbien@Sarahs-MacBook-Air.local>
2016-09-11 20:26:05 -0400

    Fixed static root

--- reachout/settings.py
+++ reachout/settings.py
@@ -125,10 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-=======
->>>>>>> 4132a12bbad7d72d7a6e20ad0b196a1336671296
 STATICFILES_DIRS = [ 
                 os.path.join(BASE_DIR, 'ratchet'),
                 os.path.join(BASE_DIR, 'template'),
