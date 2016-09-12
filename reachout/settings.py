99182c389f210388de24f60a19ebc0492329dfc3
Sarah Bien <sarahbien@Sarahs-MacBook-Air.local>
2016-09-11 20:32:48 -0400

    Change root static

--- reachout/settings.py
+++ reachout/settings.py
@@ -129,5 +129,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [ 
                 os.path.join(BASE_DIR, 'ratchet'),
                 os.path.join(BASE_DIR, 'template'),
-                os.path.join(BASE_DIR, 'static'),
                 ]
