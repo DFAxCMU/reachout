16fdc4ef944e303918c44f2ee1c2289e91294716
ljboard <ljboard@gmail.com>
2016-11-08 12:41:06 -0500

    fixed css weird behavior

---
+++ static/js/profile.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    var editNameMode = true;
+    $("#profile-totimeline").click(function() {
+        clearAll();
+        $("#profile-timeline").removeClass("hidden");
+        $("#profile-totimeline").addClass("selected-button");
+    })
+    $("#profile-toquickinfo").click(function() {
+        clearAll();
+        $("#profile-quickinfo").removeClass("hidden");
+        $("#profile-toquickinfo").addClass("selected-button");
+    })
+    $("#profile-torequests").click(function() {
+        clearAll();
+        $("#profile-requests").removeClass("hidden");
+        $("#profile-torequests").addClass("selected-button");
+    })
+
+    addRequest();
+    $("#changeNameMode").click(function() {
+        editName(editNameMode);
+        editNameMode = ! editNameMode;
+    })
+
+});
+
+function clearAll() {
+    $("#profile-timeline").addClass("hidden");
+    $("#profile-quickinfo").addClass("hidden");
+    $("#profile-requests").addClass("hidden");
+    $("#profile-totimeline").removeClass("selected-button");
+    $("#profile-toquickinfo").removeClass("selected-button");
+    $("#profile-torequests").removeClass("selected-button");
+};
+
+function addRequest() {
+    $(addrequestbutton).click(function() {
+        var newRequest = $(document.createElement('li'));
+        newRequest.addClass("table-view-cell");
+        var inputRequest = $(document.createElement(''))
+        $("#requestlist").prepend(newRequest);
+    })
+};
+
+function editName(editNameMode) {
+    if (editNameMode) {
+        $("#nameformdiv").removeClass("hidden");
+        $("#namedisplaydiv").addClass("hidden");
+        $("#tags").addClass("hidden");
+        $("#changeNameMode").removeClass("icon-compose");
+        $("#changeNameMode").addClass("icon-check");
+    } else {
+        $("#nameformdiv").addClass("hidden");
+        $("#namedisplaydiv").removeClass("hidden");
+        $("#tags").removeClass("hidden");
+        $("#changeNameMode").addClass("icon-compose");
+        $("#changeNameMode").removeClass("icon-check");
+    }
+};
