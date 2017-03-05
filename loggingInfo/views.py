07bfe09a30e7e389403d9206ccdf4ccdc8496c9e
ljboard <ljboard@gmail.com>
2017-03-05 13:46:28 -0500

    fixed (known) security issues

--- loggingInfo/views.py
+++ loggingInfo/views.py
@@ -8,84 +8,24 @@ from django.http import HttpResponseRedirect
 from django.http import JsonResponse, HttpResponse
 from backend.models import *
 
-class UpdateQuickInfo(View):
-    print("here")
+class LogInteraction(View):
     def get(self, request, client_id):
-        print("UpdateInfo GET REquest")
-        template = "quickinfo.html"
-        # request["cid"] = client_id
-        context = {}
-        context["cid"] = client_id
-        print(request)
+        template = "log_interaction.html"
+        context = {"cid": client_id}
         return render(request, template, context)
     def post(self, request, client_id):
-        print("updateInfo POST request")
-        client = Client.objects.get(pk = client_id)
-        # client.last_name
-        client.is_military     = True if request.POST.get("is_military") == "yes" else False
-        client.health_concerns = True if request.POST.get("health_concerns") == "yes" else False
-        client.dna_assistance  = True if request.POST.get("dna_assistance") == "yes" else False
-        client.has_doctor      = True if request.POST.get("has_doctor") == "yes" else False
-        client.has_insurance   = True if request.POST.get("has_insurance") == "yes" else False
-        client.duration_of_homelessness = request.POST.get("duration")
-        print(client.is_military)
-        client.save()
+        print("post logINteraction")
+        client = Client.objects.get(pk=client_id)
+        print(request.user)
+        print(type(request.user))
+        # user = CustomUser.objects.filter(name=request.user)
+        print(CustomUser.objects.all()[0])
+        user = CustomUser.objects.all()[0]
+        description = request.POST.get("description")
+        title = "" #take this out of the model?
+        new_interaction = Interaction(description=description, 
+                                      title=title, 
+                                      client=client, 
+                                      user=user)
+        new_interaction.save()
         return HttpResponseRedirect("/client/" + str(client_id))
-
-# <<<<<<< HEAD
-# def ShortQuestions(request, client_id):
-#     q1 =  request.POST.get("q1")
-#     q2 =  request.POST.get("q2")
-#     q3 =  request.POST.get("q3")
-#     q4 =  request.POST.get("q4")
-#     q5 =  request.POST.get("q5")
-#     client = Client.objects.get(pk = client_id)
-#     client.is_military = True if q1 == "yes" else False
-#     client.health_concerns = True if q2 == "yes" else False
-#     client.dna_assistance = True if q3 == "yes" else False
-#     client.has_doctor = True if q4 == "yes" else False
-#     client.duration_of_homelessness = True if q5 == "yes" else False
-#     client.save()
-
-#     context = {}
-#     context["cid"] = client_id
-#     context["requests"] = client.request.all()
-#     return render(request, 'logging-info-3.html', context)
-# =======
-    # print("title " + title)
-    # print("description " + description)
-    # client = Client.objects.get(pk = client_id)
-    # i = Interaction(description=description, title=title, client = client, user = request.user.customuser)
-    # print("Interaction", i)
-    # i.save()
-    # context = {}
-    # context["cid"] = client_id
-    # return JsonResponse({"worked": True})
-
-def ShortQuestions(request, client_id):
-    q1 =  request.POST.get("q1")
-    q2 =  request.POST.get("q2")
-    q3 =  request.POST.get("q3")
-    q4 =  request.POST.get("q4")
-    q5 =  request.POST.get("q5")
-    client = Client.objects.get(pk = client_id)
-    client.is_military = True if q1 == "yes" else False
-    client.health_concerns = True if q2 == "yes" else False
-    client.dna_assistance = True if q3 == "yes" else False
-    client.has_doctor = True if q4 == "yes" else False
-    client.has_insurance = True if q5 == "yes" else False
-    client.save()
-# >>>>>>> 881e2e177b08671a356471b8104293523d417ee5
-
-# def LogAllInfo(request, client_id):
-#     item_name =  request.POST.get("item_name")
-#     item_amount =  request.POST.get("amount")
-#     print("--------------------")
-#     print(item_name)
-#     print(item_amount)
-#     i = Item(name = item_name, amount = item_amount)
-#     i.save()  
-#     client = Client.objects.get(pk = client_id)
-#     request = Requests(item = i, client_profile = client)
-#     request.save()
-#     return JsonResponse({"worked": True})   
--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth.decorators import login_required
 from user_accounts.views import *
 # Create your views here.
 urlpatterns = [
-    url(r'^$', HomePage.as_view()),
+    url(r'^$', login_required(HomePage.as_view())),
     url(r'^login$', Login.as_view()),
-    url(r'^logout$', Logout.as_view()),
+    url(r'^logout$', login_required(Logout.as_view())),
     url(r'^register$', Register.as_view()),
     #url(r'^user$', login_required(UserPage.as_view()))
 ]
