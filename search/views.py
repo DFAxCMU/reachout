472d55fb2b43bd32fe1ef264f99faabf80ebd266
ljboard <ljboard@gmail.com>
2017-03-28 00:01:35 -0400

    issue #17

--- search/views.py
+++ search/views.py
@@ -11,47 +11,44 @@ from backend.models import Tag
 class Search(View):
     def get(self, request):
         print("Search Page: GET request")
-        full_tags_list   = Tag.objects.all()
-        client_list = Client.objects.all()
         template = "search.html"
+        client_list = Client.objects.all()
 
         search_input = request.POST.get("search_input")
         if (search_input != None): 
             search_input = search_input.lower()
 
-        context = {"client_list": client_list, 
-                   "search_input": search_input}
+        context = {"client_list": client_list}
         return render(request, template, context)
 
     def post(self, request):
+        print("Search Page: POST request")
         full_client_list = Client.objects.all()
-        full_tags_list   = Tag.objects.all()
         client_list      = []
         template         = "search.html"
 
         search_input = (request.POST.get('search_input'))
         if (search_input != None): search_input = search_input.lower()
 
-        if (search_input == ""): # reset  list
+        if (search_input == ""): # reset list
             search_input = "enter tags"
             client_list  = full_client_list
 
         for client in full_client_list: 
-            print("checking client " + client.first_name)
             hasFirst = (client.first_name.lower() == search_input)
             hasLast  = (client.last_name.lower()  == search_input)
             hasNick  = (client.nick_name.lower()  == search_input)
 
             if (hasFirst or hasLast or hasNick):
-                print("  adding client by info")
                 client_list.append(client)
 
             for tag in client.get_tags(): 
-                if (tag == search_input and client not in client_list):
-                    print("  adding client by tag: " + tag)
+                if (client not in client_list and 
+                    tag == search_input or 
+                    tag in search_input.split() or 
+                    search_input in tag.split()):
                     client_list.append(client)
 
 
-        context = {"client_list": client_list, 
-                   "search_input": search_input}
+        context = {"client_list": client_list}
         return render(request, template, context)
