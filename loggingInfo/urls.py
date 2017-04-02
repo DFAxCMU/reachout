9e198f7410210e680313577e2c1c62c968d6ba2f
Edward Dryer <edryer@Edwards-MacBook-Pro.local>
2017-04-01 20:37:22 -0400

    added interaction editing #9

--- loggingInfo/urls.py
+++ loggingInfo/urls.py
@@ -6,4 +6,5 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^log_interaction/(?P<client_id>[a-zA-Z0-9_.-]+)', login_required(LogInteraction.as_view())),
+    url(r'^edit_interaction/(?P<client_id>[a-zA-Z0-9_.-]+)/(?P<interaction_id>[a-zA-Z0-9_.-]+)', login_required(EditInteraction.as_view())),
 ]
